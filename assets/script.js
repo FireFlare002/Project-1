8d984bf7f5d18fe6ed80d41a700d04257163d739
Ubtene <uz6@scarletmail.rutgers.edu>
2017-03-16 11:11:44 -0400

    lots of change

--- assets/script.js
+++ assets/script.js
@@ -13,11 +13,8 @@ var database = firebase.database();
 var firstArray =[];
 
 
-
-
 database.ref().on("child_added", function(snapshot) {
 
-  
   
   firstArray.push(snapshot.val().Input);
 
@@ -33,20 +30,18 @@ database.ref().on("child_added", function(snapshot) {
     del.attr( 'data-buttons' , firstArray[i]);
     container.append(del);
     container.append(items);
-	$("#flex-box").prepend(container);
-    }
+
+
+  $("#flex-box").prepend(container);
+  
+  }
 
 
 
 $('.button-item').on('click', function(event) {
 
-    $(this).toggleClass('button-item-selected');
+     $(this).toggleClass('button-item-selected');
    });
-
-
-
-
-
 
 
  $(document).on("click" , ".delete" , function () {
@@ -54,11 +49,6 @@ $('.button-item').on('click', function(event) {
   var itemID = $(this).attr('data-buttons');  
 
  $("." +itemID).empty();
-
- var removedItem = database.ref().child(input);
-
- removedItem.remove();
-
                             
  });  
 
@@ -117,35 +107,124 @@ Input: input,
 
 else {
 
-
-$(document.body).append("<div id='shadow' style='position:fixed;left:0px;top:0px;width:100%; height:100%; background:gainsboro; opacity: 0.4;'></div>")
-
+$(document.body).append("<div class='shadow' id='shadow' style='position:fixed;left:0px;top:0px;width:100%; height:100%; background:gainsboro; opacity: 0.4;'></div>");
 $(".windows-popup").show(100);
 
 $("#pop-button").on("click" , function () {
 
-$(".windows-popup").hide();  
-$("#shadow").hide();
+$(".windows-popup").hide();
+$("#shadow").remove();
 
 });
 
 }
 
+});
+
+
+
+
+
+$("#login-icon").on("click" , function () {
+
+
+$(document.body).append("<div id='shadow' style='position:fixed;left:0px;top:0px;width:100%; height:100%; background:gainsboro; opacity: 0.4;'></div>");
+
+$("#popupContact").show(100);
+
+});
+
+var name = $("#name-input").val();
+
+
+
+
+
+var comment = $("#comment-input").val();
+
+
+//Login Users 
+
+
+const auth = firebase.auth()
+
+
+$("#submit-signUp").click(
+
+function () {
+
+var email = $("#email-input").val();
+var pass = $('input:password').val();	
+
+ const promise = auth.createUserWithEmailAndPassword(email, pass);
+
+if(  promise.catch(e => console.log(e.message)) ) {
+
+alert("Email taken, please input another email.");
+
+}
+
+ promise.catch(e => console.log(e.message));
+
+
 
 });
 
 
+$("#submit-login").click(
+
+function () {
+
+var email = $("#email-input").val();
+const pass = $('input:password').val();
+
+const promise = auth.signInWithEmailAndPassword(email, pass);
+
+promise.catch(e => console.log(e.message));
+
+
+
+});
+
+
+ $("#submit-logout").on("click" , function() {
+
+
+auth.signOut();
+
+
+ });
+
+
+
+
+
+ firebase.auth().onAuthStateChanged(firebaseUser => {
+
+if (firebaseUser) {
+
+ console.log(firebaseUser)
+
+ }  else {
+
+ 
+  console.log("problem");
+
+ }
+
+ });
+
 //Ajax calls
 
- $.ajax({
-     url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients', // The URL to the API. You can get this in the API page of the API you intend to consume
-     type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-     data: {}, // Additional parameters here
-     dataType: 'json',
-     success: function(data) { console.dir((data.source)); },
-     error: function(err) { alert(err); },
-     beforeSend: function(xhr) {
-     xhr.setRequestHeader("X-Mashape-Authorization", "OeA9zYKXGCmshtbXfBTFYCxry6BWp1HRLTzjsn8QLMm8dbmC0H"); // Enter here your Mashape key
-     }
- });
+ // $.ajax({
+ //     url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients', // The URL to the API. You can get this in the API page of the API you intend to consume
+ //     type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+ //     data: {}, // Additional parameters here
+ //     dataType: 'json',
+ //     success: function(data) { console.dir((data.source)); },
+ //     error: function(err) { alert(err); },
+ //     beforeSend: function(xhr) {
+ //     xhr.setRequestHeader("X-Mashape-Authorization", "OeA9zYKXGCmshtbXfBTFYCxry6BWp1HRLTzjsn8QLMm8dbmC0H"); // Enter here your Mashape key
+ //     }
+ // });
 
